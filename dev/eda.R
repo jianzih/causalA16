e07f57b79cc015ac7b906914eac7daadd02f7027
Jianzi <he.1009@osu.edu>
2016-09-18 17:52:47 -0400

    Data analysis started
    Started to analyze original dataset in replication project

--- dev/eda.R
+++ dev/eda.R
@@ -7,3 +7,12 @@ library(texreg)
 
 data1 <- load_dataset("Ross_full")
 data2 <- load_dataset("Ross_5_year")
+
+# selective decriptive statistics
+attach(data2)
+x <- data.frame(logCMRunicef,logIMRunicef,Polity_1,logDEMYRS_1,logGDPcap_1,logDen_1,GDPgrowth_1,logHIV_1)
+desc.1 <- describe(x)
+
+library(xtable)
+xtable(desc.1,skew=F)
+
