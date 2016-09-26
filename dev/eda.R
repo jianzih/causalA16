e78ceed84e026c87b4873d56f59f6d4f8c754530
Jianzi <he.1009@osu.edu>
2016-09-26 14:26:35 -0400

    Update
    Make sure all changes in last week have been uploaded.

--- dev/eda.R
+++ dev/eda.R
@@ -4,6 +4,7 @@ library(causalA16)
 library(data.table)
 library(ggplot2)
 library(texreg)
+library(plm)
 
 data1 <- load_dataset("Ross_full")
 data2 <- load_dataset("Ross_5_year")
@@ -16,3 +17,5 @@ desc.1 <- describe(x)
 library(xtable)
 xtable(desc.1,skew=F)
 
+m5 <- plm(logCMRunicef ~ Polity_1 + logHIV_1 + logGDPcap_1 + logDen_1 + GDPgrowth_1, data=data2, index=c("id", "period"), model="within", effect = "twoways")
+summary(m5)
